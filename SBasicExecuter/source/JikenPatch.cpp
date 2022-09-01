ff02db43e4988207df04cdc527d4e9018d0646d4
kuran_kuran <kuran_kuran@ka2.so-net.ne.jp>
2022-09-01 13:35:27 +0900

    Executer, パッチの矩形表示の座標がずれていたので修正した

--- SBasicExecuter/source/JikenPatch.cpp
+++ SBasicExecuter/source/JikenPatch.cpp
@@ -1,5 +1,8 @@
+// ソフトハウス殺人事件用のパッチ
+// Basic.cppのMainLoop_Setup()でJikenPatch()を実行してください
 #include "Executer.hpp"
 
+// 元は640x200だが画面モードが320x200なのでそちらに最適化
 void DrawRect2(dms::String* parameter)
 {
 	dms::Variable x = Executer::GetInstance()->Peek(0xFF64); // X(8ドット単位)
@@ -14,9 +17,9 @@ void DrawRect2(dms::String* parameter)
 	static unsigned int maskTable[3] = { 0xFF0000FF, 0xFFFF0000, 0xFF00FF00 };
 	for(int yy = 0; yy < h.GetInt(); ++ yy)
 	{
-		for(int xx = 0; xx < w.GetInt() / 2; ++ xx)
+		for(int xx = 0; xx < w.GetInt(); ++ xx)
 		{
-			int drawX = x.GetInt() * 8 + xx * 8;
+			int drawX = x.GetInt() * 4 + xx * 4;
 			int drawY = y.GetInt() + yy;
 			for(int i = 0; i < 3; ++ i)
 			{
@@ -24,13 +27,13 @@ void DrawRect2(dms::String* parameter)
 				switch(i)
 				{
 				case 0:
-					drawPattern = b.GetInt() & 0xFF;
+					drawPattern = b.GetInt() & 0xF0;
 					break;
 				case 1:
-					drawPattern = r.GetInt() & 0xFF;
+					drawPattern = r.GetInt() & 0xF0;
 					break;
 				case 2:
-					drawPattern = g.GetInt() & 0xFF;
+					drawPattern = g.GetInt() & 0xF0;
 					break;
 				}
 				screen.DrawPattern(drawX, drawY, drawPattern, 0xFFFFFFFF, maskTable[i]);
